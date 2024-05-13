cec4f51ff4d9f311ab92ab0f0ad40ae44a16045b
Sam Estrin <sam.estrin@gmail.com>
2024-05-13 13:47:09 -0700

    Update convert-image.ts

--- netlify/edge-functions/convert-image.ts
+++ netlify/edge-functions/convert-image.ts
@@ -32,25 +32,13 @@ export default async (request: Request) => {
         });
       }
 
-      // Create temporary files for input and output
-      const inputFile = await Deno.makeTempFile();
-      const outputFile = `${inputFile}.${targetFormat.toLowerCase()}`;
-
-      // Write the input file data
-      await Deno.writeFile(inputFile, fileData);
-      console.log("1");
-      // Read and convert the image
-      await ImageMagick.read(inputFile, async (image: MagickImage) => {
-        console.log("2");
-        await image.create(outputFile);
-      });
-
-      // Read the converted image data
-      const converted = await Deno.readFile(outputFile);
-
-      // Cleanup temporary files
-      await Deno.remove(inputFile);
-      await Deno.remove(outputFile);
+      const converted = await ImageMagick.read(
+        fileData,
+        async (image: MagickImage) => {
+          const result = new Uint8Array(await image.create(targetFormatEnum));
+          return result;
+        }
+      );
 
       return new Response(converted, {
         status: 200,
